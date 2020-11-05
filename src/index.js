403bc06a71bf3fbecd8afd762e4db4d4f17f0832
Nathan <nathan.ly.tang@gmail.com>
2020-11-05 16:46:53 -0700

    Change location

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const path = require("path");
+var env = JSON.parse(fs.readFileSync(path.join(__dirname, "env.json")));
+var logo = path.join(__dirname, "..\assets\logo.png");
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({ activity: { name: '-status | -help', type: 'LISTENING' }, status: 'online' })
+        // .then(console.log)
+        .catch(console.error);
+});
+
+client.on('message', message => {
+    if (message.content === '-mc help') {
+        const helpEmbed = new Discord.MessageEmbed()
+            .setColor('#62B36F')
+            .setAuthor('Steve', 'https://i.imgur.com/gb5oeQt.png')
+            .setDescription(`Steve is a Discord bot for Minecraft communities!  
+Make it easy to get your server IP and server status.`)
+            .addFields(
+                { name: 'Commands', value: '-mc help\n-mc status\n-mc ip\n\u200B', inline: true },
+                { name: '\u200B', value: '\u200B', inline: true },
+                { name: 'Description', value: 'Display this message\nGet the server status\nGet the server IP address\n\u200B', inline: true },
+            )
+            .setFooter('Made by Alienics 👾')
+        message.channel.send(helpEmbed);
+    }
+});
+
+client.login(env.token);
