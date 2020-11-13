77cdfe15cdba2bb6402163e48dcde3ae3beaa59f
Nathan <nathan.ly.tang@gmail.com>
2020-11-13 03:15:02 -0700

    Add grab skin command

--- src/index.js
+++ src/index.js
@@ -29,9 +29,9 @@ client.on('message', message => {
             .setAuthor('Steve', 'https://i.imgur.com/gb5oeQt.png')
             .setDescription(`Steve is a Discord bot for Minecraft communities!\nMake it easy to get your server IP and server status.`)
             .addFields(
-                { name: 'Commands', value: '-mc help\n-mc status\n-mc ip\n\u200B', inline: true },
+                { name: 'Commands', value: `${prefix}help\n${prefix}status\n${prefix}ip\n${prefix}skin <user>\n\u200B`, inline: true },
                 { name: '\u200B', value: '\u200B', inline: true },
-                { name: 'Description', value: 'Display this message\nGet the server status\nGet the server IP address\n\u200B', inline: true },
+                { name: 'Description', value: 'Display this message\nGet the server status\nGet the server IP address\nGet a username\'s MC skin\n\u200B', inline: true },
             )
             .setFooter('Made by Alienics 👾')
         message.channel.send(helpEmbed);
@@ -47,9 +47,9 @@ client.on('message', message => {
             if (status.status && status.online) {
 
                 // Get player list
-                var playerList = 'None';
+                var playerList = 'No current players';
                 if (status.players.online > 0) {
-                    var playerList = '';
+                    var playerList = ``;
                     for (var i = 0; i < status.players.online; i++) {
                         if (i % 4 == 0) {
                             playerList += `\n`;
@@ -58,11 +58,11 @@ client.on('message', message => {
                     }
                     playerList = playerList.substring(0, playerList.length - 2);
                 }
-                
+
                 // Create and send embed
                 const statusEmbed = new Discord.MessageEmbed()
                     .setColor('#2ECC71')
-                    .setTitle('Server Status')
+                    .setTitle('Minecraft Server Status')
                     .addFields(
                         { name: 'Status', value: `Online\n`, inline: true },
                         { name: 'Version', value: `${status.version.name}\n`, inline: true },
@@ -119,6 +119,35 @@ client.on('message', message => {
         //     .setFooter(`${env.footer}`)
         // message.channel.send(statusEmbed);
 
+    }
+
+    if (command === 'skin') {
+        if (args.length == 0) {
+            return message.channel.send(`You didn't provide a username, ${message.author}!`);
+        } else if (args.length > 1) {
+            return message.channel.send(`Too many arguments! Please input only one username, ${message.author}`);
+        }
+
+        (async () => {
+            // Get UUID from API
+            const response = await fetch(`https://playerdb.co/api/player/minecraft/${args[0]}`,);
+            const playerInfo = await response.json();
+
+            if (playerInfo.code === 'minecraft.api_failure') {
+                return message.channel.send(`${args[0]} is not a valid Minecraft username, ${message.author}!`);
+            }
+
+            const skinEmbed = new Discord.MessageEmbed()
+                .setColor('#62B36F')
+                .setTitle(`${playerInfo.data.player.username}'s Minecraft Skin`)
+                .setThumbnail(`https://crafatar.com/renders/body/${playerInfo.data.player.id}?overlay=true`)
+                .setImage(`https://crafatar.com/skins/${playerInfo.data.player.id}`)
+                .addFields(
+                    { name: 'Download', value: `To download this skin, click [here](https://minecraft.tools/download-skin/${playerInfo.data.player.username} "${playerInfo.data.player.username}'s skin").\n`, inline: true },
+                )
+            message.channel.send(skinEmbed);
+
+        })();
     }
 
 });
