ff27bdac55e1be28d35a316ddfe7b004cd6310c6
Nathan <nathan.ly.tang@gmail.com>
2021-01-19 21:58:09 -0700

    Remove extraneous code

--- src/lib/leave.js
+++ src/lib/leave.js
@@ -16,11 +16,6 @@ module.exports = {
                 .setAuthor('Steve', 'https://i.imgur.com/gb5oeQt.png')
                 .setDescription(`Goodbye! Click [here](${invite}) to invite me again.`)
             await message.channel.send(leaveEmbed);
-            
-            // Delete server properties from JSON and leave guild
-            delete env[serverID];
-            fs.writeFileSync(path.join(__dirname, data), JSON.stringify(env));
-            console.log(`Left guild: ${message.guild.id} (${message.guild.name})`);
             message.guild.leave();
         })();
         return;
