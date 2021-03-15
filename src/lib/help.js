093d7de0d6b374ba0d25dd2cecb292db92d4680d
Nathan <nathan.ly.tang@gmail.com>
2021-03-15 03:16:49 -0600

    Refactor: Update function variables

--- src/lib/help.js
+++ src/lib/help.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'help',
     aliases: ['commands'],
 	description: 'Steve Help Command',
-	execute(Discord, env, serverID, message, args, invite) {
+	execute(Discord, pool, serverID, message, args, invite, prefix) {
         console.log(`Server ${serverID} (${message.guild.name}) sent help command`);
 
         // Create and send help embed
@@ -11,7 +11,7 @@ module.exports = {
             .setAuthor('Steve', 'https://i.imgur.com/gb5oeQt.png')
             .setDescription(`Steve is a Discord bot for Minecraft communities! Make it easy \nto get your server address and server status all in one panel.`)
             .addFields(
-                { name: 'Commands', value: `${env[serverID].prefix}help\n${env[serverID].prefix}setup\n${env[serverID].prefix}settings\n${env[serverID].prefix}status\n${env[serverID].prefix}ip\n${env[serverID].prefix}skin <user>\n${env[serverID].prefix}leave\n`, inline: true },
+                { name: 'Commands', value: `${prefix}help\n${prefix}setup\n${prefix}settings\n${prefix}status\n${prefix}ip\n${prefix}skin <user>\n${prefix}leave\n`, inline: true },
                 // { name: '\u200B', value: '\u200B', inline: true },
                 { name: 'Description', value: 'Display this message\nDisplay setup instructions\nDisplay current settings\nGet the server status\nGet the server IP address\nGet a username\'s MC skin\nSteve will leave the server\n', inline: true },
             )
