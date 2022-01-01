a4eaa349f293c0f6cd8cf0b48ddc31a99b80e15d
Nathan Tang <nathan.ly.tang@gmail.com>
2021-12-31 20:03:53 -0700

    Ref: Remove client message listeners, drop prefix column

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const commandFiles = fs.readdirSync(path.join(__dirname, 'lib')).filter(file => file.endsWith('.js'));
 var invite;
-const prefix = '-mc ';
-const clientId = null;
-const guildId = null;
+var clientId;
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 const commands = [];
@@ -32,8 +30,11 @@ const commands = [];
 
 if (version === 'production') {
     client.login(); // Production build
+    clientId = process.env.CLIENT_ID;
 } else {
     client.login(process.env.NIGHTLY); // Nightly build
+    clientId = process.env.DEV_CLIENT;
+    var guildId = process.env.DEV_GUILD;
 }
 
 client.on("ready", () => {
@@ -54,22 +55,17 @@ client.on("ready", () => {
             console.log(err);
         });
 
-    // guild_list = client.guilds.cache.map(guild => guild.id);
-
     console.log(`Logged in as ${client.user.tag}! Monitoring ${client.guilds.cache.size} servers.`);
-    client.user.setPresence({ activities: [{ name: `${prefix}status | ${prefix}help`, type: 'LISTENING' }] });
+    client.user.setPresence({ activities: [{ name: `/status | /help`, type: 'LISTENING' }] });
     client.user.setStatus("online");
     invite = `https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=2147502080&scope=applications.commands%20bot`;
 
-
-    const rest = new REST({ version: '9' }).setToken(process.env.NIGHTLY);
+    // Register guild commands
+    const rest = new REST({ version: '9' }).setToken(version === 'production' ? process.env.DISCORD_TOKEN : process.env.NIGHTLY);
     (async () => {
         try {
-            if (guildId) {
-                await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
-            }
+            version === 'production' ? await rest.put(Routes.applicationCommands(clientId), { body: commands }) : await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
         } catch (err) {
-            console.log("failed");
             console.error(err);
         }
     })();
@@ -87,9 +83,9 @@ client.on("guildCreate", (guild) => {
         // Send welcome embed message
         const welcomeEmbed = new Discord.MessageEmbed()
             .setColor('#62B36F')
-            .setAuthor('Steve', 'https://i.imgur.com/gb5oeQt.png')
-            .setDescription(`Hello! I'm Steve, a bot designed to get and display your Minecraft server status!  Thanks for adding me to your server.  To view all my available commands, use \`${prefix} help\`.`)
-            .setFooter('Made by Alienics#5796 👾');
+            .setAuthor({ name: 'Steve', iconURL: 'https://i.imgur.com/gb5oeQt.png' })
+            .setDescription(`Hello! I'm Steve, a bot designed to get and display your Minecraft server status!  Thanks for adding me to your server.  To view all my available commands, use \`/help\`.`)
+            .setFooter({ text: 'Made by Alienics#5796 👾' });
         guild.systemChannel.send({ embeds: [welcomeEmbed] });
     } catch {
         // System channel not enabled
@@ -103,68 +99,27 @@ client.on("guildDelete", (guild) => {
     console.log(`Left guild: ${guild.id} (${guild.name})`);
 
     // Remove guild data from database
-    const serverID = guild.id.toString();
     let sql = "DELETE FROM guild_data WHERE guild_id = ?;";
-    let vars = [serverID];
+    let vars = [guild.id];
     const remove_guild = new SQL_Query(pool, sql, vars);
     remove_guild.query();
     return;
 });
-
-// TODO: Check if command is coming from interaction or message in lib, and use interaction reply or message.channel.send accordingly
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
     const command = client.commands.get(interaction.commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(interaction.commandName));
     if (!command) return; // Check if command in commands folder
 
-    // Check if author has permission to execute commands
-    if (!checkUserHasPermissions("interaction", command.permissions, interaction)) return;
-
-    await interaction.reply(`${interaction.commandName} command was logged`);
-    // // Command handler
-    // try {
-    //     const args = {
-    //         "discord": Discord,
-    //         "pool": pool,
-    //         "serverID": interaction.guildId,
-    //         "interaction": interaction,
-    //         "invite": invite,
-    //         "prefix": prefix
-    //     };
-    //     command.execute(pool, serverID, message, args, invite, prefix);
-    // } catch (error) {
-    //     console.error(error);
-    //     return interaction.reply({ content: 'There was an error trying to execute that command!' });
-    // }
-});
-
-client.on("messageCreate", async message => {
-
-    // Check if message has bot prefix / mentions bot
-    let sliceLen;
-    if (message.content.startsWith(prefix)) {
-        sliceLen = prefix.length;
-    } else if (message.mentions.has(client.user.id) && !message.mentions.everyone) {
-        sliceLen = 23;
-    } else {
-        return;
-    }
-
-    if (message.channel.type == "dm") return; // Ignore direct messages
-
-    if (!checkBotHasPermissions(message.guild)) return; // Check if bot has permissions
-
     // Get guild ID and check if guild in database
-    var serverID = message.guild.id.toString();
     let sql = ' SELECT * FROM guild_data WHERE guild_id = ? ';
-    let vars = [serverID];
+    let vars = [interaction.guildId];
     let guild_exists = new SQL_Query(pool, sql, vars);
-    guild_exists.query()
-        .then((rows) => {
+    await guild_exists.query()
+        .then(async (rows) => {
             if (rows.length === 0) {
-                console.log(`Server ${serverID} (${message.guild.name}): Guild not found in database.  Adding to database.`);
-                addGuildToData(message.guild);
+                console.log(`Server ${interaction.guildId} (${interaction.guild.name}): Guild not found in database.  Adding to database.`);
+                await addGuildToData(interaction.guild);
             }
         })
         .catch((err) => {
@@ -172,53 +127,39 @@ client.on("messageCreate", async message => {
             console.log(err);
         });
 
-    // Separate command and arguments
-    const commandBody = message.content.slice(sliceLen);
-    const args = commandBody.split(' ');
-    const commandName = args.shift().toLowerCase();
-
-    // Grab commands and aliases
-    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-    if (!command) return; // Check if command in commands folder
-
     // Check if author has permission to execute commands
-    if (!checkUserHasPermissions("message", command.permissions, message)) return;
+    if (!checkUserHasPermissions("interaction", command.permissions, interaction)) return;
 
     // Command handler
     try {
-        command.execute(Discord, pool, serverID, message, args, invite, prefix);
+        await command.execute(pool, interaction.guildId, interaction, invite);
     } catch (error) {
         console.error(error);
-        return message.channel.send({ content: 'There was an error trying to execute that command!' });
+        return interaction.reply({ content: 'There was an error trying to execute that command!' });
     }
-
 });
 
-function addGuildToData(guild) {
+async function addGuildToData(guild) {
     // Create new row in guild_data table with guild ID as primary key
-    let serverID = guild.id.toString();
     let sql = "INSERT INTO guild_data VALUES (?, 1, '25565', '', '', '', '-mc ');";
-    let vars = [serverID];
+    let vars = [guild.id];
     const add_guild = new SQL_Query(pool, sql, vars);
-    add_guild.query()
+    return await add_guild.query()
         .catch((err) => {
-            console.log(`\x1b[31m\x1b[1mError adding guild to database for guild ${serverID} (${guild.name}):\x1b[0m`);
+            console.log(`\x1b[31m\x1b[1mError adding guild to database for guild ${guild.id} (${guild.name}):\x1b[0m`);
             console.log(err);
-            return;
         });
-    return;
 }
 
 function createTable() {
     // Create new table if not already existing in database
     let sql = `CREATE TABLE IF NOT EXISTS guild_data (
-        guild_id VARCHAR(18) NOT NULL,   
-        query BOOLEAN NOT NULL DEFAULT 1,          
-        port VARCHAR(5) NOT NULL DEFAULT '25565',   
-        url VARCHAR(2048) DEFAULT '',   
-        name VARCHAR(50) DEFAULT '',   
-        footer VARCHAR(50) DEFAULT '',   
-        prefix VARCHAR(10) NOT NULL DEFAULT '-mc ',   
+        guild_id VARCHAR(18) NOT NULL,
+        query BOOLEAN NOT NULL DEFAULT 1,
+        port VARCHAR(5) NOT NULL DEFAULT '25565',
+        url VARCHAR(2048) DEFAULT '',
+        name VARCHAR(50) DEFAULT '',
+        footer VARCHAR(50) DEFAULT '',
         PRIMARY KEY ( guild_id )) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;`;
     let create_table = new SQL_Query(pool, sql);
     create_table.query()
@@ -230,7 +171,6 @@ function createTable() {
 }
 
 function checkBotHasPermissions(guild) {
-    console.log(guild.me.permissions.serialize());
     // Check if bot has permissions
     if (!guild.me.permissions.has(Permissions.FLAGS.SEND_MESSAGES)) {
         console.log(`Server ${guild.id.toString()} (${guild.name}): No permission to send messages.`);
@@ -247,7 +187,7 @@ function checkBotHasPermissions(guild) {
 function checkUserHasPermissions(type, permissions, messageType) {
     const adminEmbed = new Discord.MessageEmbed()
         .setColor('#E74C3C')
-        .setAuthor('Steve', 'https://i.imgur.com/gb5oeQt.png')
+        .setAuthor({ name: 'Steve', iconURL: 'https://i.imgur.com/gb5oeQt.png' })
         .setDescription(`Only administrators can make changes to Steve!`);
 
     if (type === 'interaction') {
@@ -270,7 +210,7 @@ function checkUserHasPermissions(type, permissions, messageType) {
 
 // Refresh client presence every hour
 setInterval(() => {
-    client.user.setPresence({ activities: [{ name: `${prefix}status | ${prefix}help`, type: 'LISTENING' }] });
+    client.user.setPresence({ activities: [{ name: `/status | /help`, type: 'LISTENING' }] });
     client.user.setStatus("online");
 }, 3600000);
 
