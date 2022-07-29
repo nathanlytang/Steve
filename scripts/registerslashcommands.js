8f8421d56ba238958281fce08268b550e8c309b2
Nathan Tang <nathan.ly.tang@gmail.com>
2022-07-29 01:19:37 -0600

    Feat: dev guild registration

--- scripts/registerslashcommands.js
+++ scripts/registerslashcommands.js
@@ -1,33 +1,58 @@
-import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v9';
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
 import dotenv from "dotenv";
 dotenv.config();
 import fs from "fs";
 import path from "path";
-import url from 'url';
+import url from "url";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const commands = [];
-const commandFiles = fs.readdirSync(path.join(__dirname, '../src/lib')).filter(file => file.endsWith('.js'));
-
-const clientId = process.env.CLIENT_ID;
+const commandFiles = fs
+    .readdirSync(path.join(__dirname, "../dist/src/lib"))
+    .filter((file) => file.endsWith(".js"));
 
 (async () => {
+    if (process.argv.length > 2 && process.argv[2] === "dev") {
+        // Put commands in collection
+        const rest = new REST({ version: "9" }).setToken(process.env.NIGHTLY);
+        for (const file of commandFiles) {
+            const command = await import(`../dist/src/lib/${file}`);
+            commands.push(await command.data.toJSON());
+        }
 
-	// Put commands in collection
-	const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
-	for (const file of commandFiles) {
-		const command = await import(`../src/lib/${file}`);
-		commands.push(await command.data.toJSON());
-	}
+        // Register guild commands
+        try {
+            await rest.put(
+                Routes.applicationGuildCommands(
+                    process.env.DEV_CLIENT,
+                    process.env.DEV_GUILD
+                ),
+                { body: commands }
+            );
+            console.log("Dev slash commands registered");
+        } catch (err) {
+            console.error(err);
+        }
+    } else {
+        // Put commands in collection
+        const rest = new REST({ version: "9" }).setToken(
+            process.env.DISCORD_TOKEN
+        );
+        for (const file of commandFiles) {
+            const command = await import(`../dist/src/lib/${file}`);
+            commands.push(await command.data.toJSON());
+        }
 
-	// Register guild commands
-	try {
-		await rest.put(Routes.applicationCommands(clientId), { body: commands });
-		console.log("Slash commands registered");
-	} catch (err) {
-		console.error(err);
-	}
-
+        // Register guild commands
+        try {
+            await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+                body: commands,
+            });
+            console.log("Slash commands registered");
+        } catch (err) {
+            console.error(err);
+        }
+    }
 })();
