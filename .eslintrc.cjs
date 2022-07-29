1c6cb260b0fd3cfa49bf5b09a1e08c8719dd84fd
Nathan Tang <nathan.ly.tang@gmail.com>
2022-07-29 01:18:13 -0600

    Formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,23 @@
 module.exports = {
     "env": {
         "es2021": true,
-        "node": true
+        "node": true,
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 13,
-        "sourceType": "module"
+        "sourceType": "module",
     },
     "plugins": [
-        "@typescript-eslint"
+        "@typescript-eslint",
     ],
     "rules": {
         "no-unused-vars": 0,
         "semi": "warn",
-        "eqeqeq": "always"
+        "eqeqeq": "always",
     }
 };
