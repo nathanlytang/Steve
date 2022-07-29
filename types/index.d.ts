218c2136917ebfe64296869b5dfc022ecd39ebbc
Nathan Tang <nathan.ly.tang@gmail.com>
2022-07-29 01:17:54 -0600

    Add player and skin types

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,9 +6,13 @@ export interface Command {
     name: string;
     aliases: string[];
     description: string;
-    data: SlashCommandBuilder
-    permissions: PermissionResolvable[]
+    data: SlashCommandBuilder;
+    permissions: PermissionResolvable[];
     execute: (options: CommandOptions) => null;
+}
+
+export interface Cmd {
+    aliases: string[];
 }
 
 export interface CommandOptions {
@@ -16,4 +20,18 @@ export interface CommandOptions {
     serverID: string;
     interaction: CommandInteraction;
     invite: string;
+}
+
+export interface PlayerInfo {
+    name: string;
+    id: string
+}
+
+export interface SkinInfo {
+    id: string;
+    name: string;
+    properties: {
+        name: string;
+        value: string;
+    }[]
 }
